68a3923bd372774bfcc569d201cdade10ea0abeb
mmukaigawara <mmukaigawara@gmail.com>
2023-01-09 19:16:53 -0500

    code for analysis

---
+++ Code/Analysis.R
@@ -0,0 +1,33 @@
+library(spatstat)
+
+setwd("~/Dropbox/Research/geocausal/")
+
+# Load data
+#set.seed(1234)
+#dta <- GetAnalysisData(subset_dates = subset_dates, use_lags = use_lags,
+#                       sof_priority_coef = sof_priority_coef,
+#                       hist_priority_coefs = hist_priority_coefs,
+#                       covs_priority_coefs = covs_priority_coefs,
+#                       df_time = df_time, jitter = jitter,
+#                       jitter_amount = jitter_amount)
+
+
+# List of covariates
+use_lags <- c(1, 7, 30) #Lags for lagged interventions
+df_time <- 3
+ps_covs <- c('logPopulation', 'prior_rivers', 'prior_roads', 'All_Cities', 'aid',
+             sapply(c('prior_PrevKinetic', 'prior_PrevAttacks'),
+                    function(x) paste0(x, '.', use_lags)),
+             sapply('prior_PrevSOF', function(x) paste0(x, '.', use_lags)),
+             paste0('time.', 1 : df_time),
+             paste0('Settle.IQ.G0', 1 : 9),
+             paste0('Settle.IQ.G', 10 : 18)) #List of covariates
+
+# Obtain counterfactual distributions
+counter.out <- counterfactual(outcome = "Kinetic",
+                              covariates = ps_covs,
+                              data = ps_dta,
+                              newdata = data,
+                              counter = 3,
+                              multiple = TRUE)
+
