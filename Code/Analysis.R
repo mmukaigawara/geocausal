b6088c0cf0dff70305acb8ddfcf93100cc269fca
mmukaigawara <mmukaigawara@gmail.com>
2023-01-09 19:22:04 -0500

    code for analysis

--- Code/Analysis.R
+++ Code/Analysis.R
@@ -1,16 +1,26 @@
 library(spatstat)
 
-setwd("~/Dropbox/Research/geocausal/")
+setwd("~/Dropbox/Research/geocausal.package/")
+
+# Load functions
 
 # Load data
-#set.seed(1234)
-#dta <- GetAnalysisData(subset_dates = subset_dates, use_lags = use_lags,
-#                       sof_priority_coef = sof_priority_coef,
-#                       hist_priority_coefs = hist_priority_coefs,
-#                       covs_priority_coefs = covs_priority_coefs,
-#                       df_time = df_time, jitter = jitter,
-#                       jitter_amount = jitter_amount)
 
+## This dataset is obtained via GetAnalysisData function (for example, see get_ps.R)
+
+##set.seed(1234)
+
+##dta <- GetAnalysisData(subset_dates = subset_dates, use_lags = use_lags,
+##                       sof_priority_coef = sof_priority_coef,
+##                       hist_priority_coefs = hist_priority_coefs,
+##                       covs_priority_coefs = covs_priority_coefs,
+##                       df_time = df_time, jitter = jitter,
+##                       jitter_amount = jitter_amount)
+
+##ps_dta <- subset(dta$ps_dta, date %in% dta$keep_dates)
+
+load("~/Dropbox/Research/geocausal.package/Data/ps_dta.RData")
+source('~/Dropbox/Research/geocausal.package/Function/function_counterfactual.R')
 
 # List of covariates
 use_lags <- c(1, 7, 30) #Lags for lagged interventions
@@ -23,7 +33,7 @@ ps_covs <- c('logPopulation', 'prior_rivers', 'prior_roads', 'All_Cities', 'aid'
              paste0('Settle.IQ.G0', 1 : 9),
              paste0('Settle.IQ.G', 10 : 18)) #List of covariates
 
-# Obtain counterfactual distributions
+# Obtain actual and counterfactual distributions
 counter.out <- counterfactual(outcome = "Kinetic",
                               covariates = ps_covs,
                               data = ps_dta,
@@ -31,3 +41,6 @@ counter.out <- counterfactual(outcome = "Kinetic",
                               counter = 3,
                               multiple = TRUE)
 
+# Plot actual and counterfactual distributions
+plot(counter.out[[1]][[1]]) #Actual
+plot(counter.out[[2]][[1]]) #Counterfactual
