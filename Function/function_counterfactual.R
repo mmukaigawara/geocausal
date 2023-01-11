06a7275a94c0ce893a4344ddd0b9b97c4b26ab7a
mmukaigawara <mmukaigawara@gmail.com>
2023-01-11 18:49:21 -0500

    fasten the process by omitting the diagnostic part

--- Function/function_counterfactual.R
+++ Function/function_counterfactual.R
@@ -10,13 +10,13 @@
 # covariates: a vector of names of covariates
 # data: data to fit the poisson model
 # newdata: data used for prediction; by default data = newdata
-# counter: a constant to multiply the baseline density
+# counter: a constant to multiply the baseline density. By default = 1 and returns the original distribution
 # scenario: "intensity" or "location"; if "intensity," this fx allows users to consider counterfactuals wrt intensity
 #           if "location", it allows users to consider counterfactuals wrt locations (at this moment, intensity only)
 
 counterfactual <- function(outcome, covariates, data,
                            newdata = data,
-                           counter = NULL,
+                           counter = 1,
                            multiple = TRUE,
                            scenario = "intensity"){
   
@@ -40,32 +40,14 @@ counterfactual <- function(outcome, covariates, data,
   }
   
   # Actual and counterfactual intensities -----
-  if (identical(newdata,data) == TRUE){
-    
-    # Conditional intensity with the actual distribution
-    fitted_ps <- predict.mppm(ps_mod, type = "cif", data, ngrid = 100)$cif
-    
-    # Conditional intensity with a counterfactual distribution (multiplied by counter)
-    fitted_ps_counter <- counter * fitted_ps
-    
-    return(list(fitted_ps = fitted_ps,
-                fitted_ps_counter = fitted_ps_counter)) # A list of two elements
-    
-  } else if (identical(newdata, data) == FALSE){
-    
-    # Conditional intensity with the actual distribution
-    fitted_ps <- predict.mppm(ps_mod, type = "cif", data, ngrid = 100)$cif
-    fitted_ps_newdata <- predict.mppm(ps_mod, type = "cif", newdata, ngrid = 100)$cif
-    
-    # Conditional intensity with a counterfactual distribution (multiplied by counter)
-    fitted_ps_counter <- counter * fitted_ps
-    fitted_ps_counter_newdata <- counter * fitted_ps_newdata
-    
-    return(list(fitted_ps = fitted_ps,
-                fitted_ps_counter = fitted_ps_counter,
-                fitted_ps_newdata = fitted_ps_newdata,
-                fitted_ps_counter_newdata = fitted_ps_counter_newdata)) # A list of four elements
   
-  }
+  ## Conditional intensity with the actual distribution
+  fitted_ps <- predict.mppm(ps_mod, type = "cif", newdata, ngrid = 100)$cif
   
+  ## Conditional intensity with a counterfactual distribution (multiplied by counter)
+  fitted_ps_counter <- counter * fitted_ps
+  
+  return(list(fitted_ps = fitted_ps,
+              fitted_ps_counter = fitted_ps_counter)) # A list of two elements
+    
 }
