89f32455787fd1eafe03a05003d9414b831c384b
mmukaigawara <mmukaigawara@gmail.com>
2023-01-11 19:53:03 -0500

    separated out the ppm/mppm

--- Function/function_counterfactual.R
+++ Function/function_counterfactual.R
@@ -1,54 +1,44 @@
 # Function: counterfactual
 
 # This function does the following:
-# 1. Fit a poisson process model
-# 2. Fit a counterfactual model (at this moment, modify intensities only: baseline density x constant)
-# 3. Return fitted hyperframes for the original and counterfactual distributions
+# 1. Fit a counterfactual model (at this moment, modify intensities only: baseline density x constant)
+# 2. Return fitted hyperframes for the original and counterfactual distributions
 
 # Specifications:
-# outcome: an outcome variable
-# covariates: a vector of names of covariates
 # data: data to fit the poisson model
 # newdata: data used for prediction; by default data = newdata
 # counter: a constant to multiply the baseline density. By default = 1 and returns the original distribution
+# multiple: choice of ppm or mppm
 # scenario: "intensity" or "location"; if "intensity," this fx allows users to consider counterfactuals wrt intensity
 #           if "location", it allows users to consider counterfactuals wrt locations (at this moment, intensity only)
 
-counterfactual <- function(outcome, covariates, data,
+counterfactual <- function(ps_mod, data,
                            newdata = data,
-                           counter = c,
+                           counter = counter,
                            multiple = TRUE,
                            scenario = "intensity"){
   
-  # Diagnostics (data and counterfactual specifications) -----
-  if (class(data)[1] != "hyperframe") {
-    cat("Convert data to a hyperframe class object.")
-  }
-  
+  # Diagnostics (counterfactual specifications) -----
   if (counter <= 0){
     cat("Set counter > 0.")
-  }
-  
-  # Formula -----
-  ps_eq <- paste(outcome, "~", paste(ps_covs, collapse = "+"))
-  
-  # Fit the model (ppm or mppm) -----
-  if (multiple == TRUE){
-    ps_mod <- mppm(as.formula(ps_eq), data)
-  } else if (multiple == FALSE){
-    ps_mod <- ppm(as.formula(ps_eq), data)
   }
   
   # Actual and counterfactual intensities -----
   
   ## Conditional intensity with the actual distribution
+  cat("Obtaining the conditional intensity.")
+  
   fitted_ps <- predict.mppm(ps_mod, type = "cif", newdata, ngrid = 100)$cif
   
   ## Conditional intensity with a counterfactual distribution (multiplied by counter)
-  if (c == 1){ #If c = 1, just return fitted_ps
+  if (counter == 1){ #If c = 1, just return fitted_ps
+    
     return(list(fitted_ps = fitted_ps))
-  } else if (c > 1){ #If not, obtain a counterfactual distribution and return a list
+    
+  } else if (counter > 1){ #If not, obtain a counterfactual distribution and return a list
+    
     fitted_ps_counter <- counter * fitted_ps
+  
     return(list(fitted_ps = fitted_ps,
                 fitted_ps_counter = fitted_ps_counter)) # A list of two elements
   }
