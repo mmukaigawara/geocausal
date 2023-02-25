0ce8f2cd4c58960f2fb01549bc5975141000c539
mmukaigawara <mmukaigawara@gmail.com>
2023-02-25 14:08:49 -0500

    fixed the order issue, eliminated jittering

--- R/get_hfr.R
+++ R/get_hfr.R
@@ -10,34 +10,36 @@
 #' @param window Window, should be saved as an owin object
 #' @param date Name of the column for dates in the treatment dataset
 #' @param coordinates Names of columns for coordinates. By default, x = longitude and y = latitude
-#' @param jitter Indicating whether to jitter; by default TRUE
-#' @param jitter_amount Amount of jittering; by default 0.00001
 #' @param combined Whether to generate output for all treatment and all outcomes combined. By default TRUE
 
 get_hfr <- function(treatment, treatment_type,
                     outcome, outcome_type, window,
                     date = "date",
                     coordinates = c("longitude", "latitude"),
-                    jitter = TRUE, jitter_amount = 0.0001, combined = TRUE) {
+                    combined = TRUE) {
 
   # Getting the range of dates -----------
-  date_range <- base::range(treatment[, date])
-  all_dates <- seq(date_range[1], date_range[2], by = 1)
+  all_dates <- seq(min(treatment$date), max(treatment$date), by = 1)
 
   # Cleaning the data ----------
-  treatment <- treatment %>% dplyr::select(date, treatment_type, coordinates[1], coordinates[2])
-  outcome <- outcome %>% dplyr::select(date, outcome_type, coordinates[1], coordinates[2])
+  treatment <- treatment %>%
+    dplyr::select(date, treatment_type, coordinates[1], coordinates[2])
+  outcome <- outcome %>%
+    dplyr::select(date, outcome_type, coordinates[1], coordinates[2])
+
+  colnames(treatment) <- c("date", "type", "longitude", "latitude")
+  colnames(outcome) <- c("date", "type", "longitude", "latitude")
 
   setDT(treatment)
   setDT(outcome)
 
   if (combined){
     treatment_c <- treatment
-    treatment_c[, treatment_type] <- "all_treatment"
+    treatment_c[, "type"] <- "all_treatment"
     treatment <- rbind(treatment, treatment_c)
 
     outcome_c <- outcome
-    outcome_c[, outcome_type] <- "all_outcome"
+    outcome_c[, "type"] <- "all_outcome"
     outcome <- rbind(outcome, outcome_c)
   }
 
@@ -49,34 +51,26 @@ get_hfr <- function(treatment, treatment_type,
   empty_ppp <- as.ppp(cbind(x = empty$longitude, y = empty$latitude), W = window)
 
   ## Converting data to point process
-  x_ppp <- treatment %>%
-    group_by_at(vars(date, paste0(treatment_type))) %>%
-    group_map(~as.ppp(cbind(x = .x[, coordinates[1]],
-                            y = .x[, coordinates[2]]),
-                      W = window))
-
-  if (jitter) {
-    x_ppp <- map(x_ppp, spatstat.geom::rjitter, radius = jitter_amount)
-  }
+  as.list(
+    treatment[, .(list(as.ppp(cbind(x = .SD$longitude,
+                                    y = .SD$latitude),
+                              W = window))),
+              by = list(date, type)]
+  ) -> x_ppp
 
   cat("Converting the treatment data to a hyperframe...\n")
 
   ## Identifying missing dates
   all_date_type <- expand.grid(date = all_dates,
-                               type = pull(unique(treatment[, ..treatment_type])))
-
-  obs_date_type <- treatment %>%
-    group_by_at(vars(date, paste0(treatment_type))) %>%
-    dplyr::select(date, paste0(treatment_type)) %>%
-    distinct() %>%
-    rename(type = treatment_type)
-
+                               type = unique(treatment[, type]))
+  obs_date_type <- data.table(date = x_ppp$date,
+                              type = x_ppp$type)
   missing_date_type <- setdiff(all_date_type, obs_date_type)
 
   ## Combining observed and missing data
-  x_list <- c(x_ppp, rep(list(empty_ppp), nrow(missing_date_type)))
+  x_list <- c(x_ppp$V1, rep(list(empty_ppp), nrow(missing_date_type)))
   x_index <- rbind(obs_date_type, missing_date_type)
-  treatment_types <- pull(unique(treatment[, ..treatment_type]))
+  treatment_types <- unique(treatment[, type])
 
   lapply(1:length(treatment_types),
          function(ii){
@@ -87,41 +81,33 @@ get_hfr <- function(treatment, treatment_type,
 
   for(jj in 1:length(treatment_types)){
     x_hyperframe[, treatment_types[jj]] <-
-      x_list[[jj]][order(subset(x_index, type == treatment_types[[1]])$date)]
+      x_list[[jj]][order(subset(x_index, type == treatment_types[[jj]])$date)]
   }
 
   # Converting outcome data to ppp ----------
   cat("Converting the outcome data to ppp objects...\n")
 
   ## Converting data to point process
-  y_ppp <- outcome %>%
-    group_by_at(vars(date, paste0(outcome_type))) %>%
-    group_map(~as.ppp(cbind(x = .x[, coordinates[1]],
-                            y = .x[, coordinates[2]]),
-                      W = window))
-
-  if (jitter) {
-    y_ppp <- map(y_ppp, spatstat.geom::rjitter, radius = jitter_amount)
-  }
+  as.list(
+    outcome[, .(list(as.ppp(cbind(x = .SD$longitude,
+                                  y = .SD$latitude),
+                            W = window))),
+            by = list(date, type)]
+  ) -> y_ppp
 
   cat("Converting the outcome data to a hyperframe...\n")
 
   ## Identifying missing dates
-  all_date_type_y <- expand.grid(date = all_dates,
-                                 type = pull(unique(outcome[, ..outcome_type])))
-
-  obs_date_type_y <- outcome %>%
-    group_by_at(vars(date, paste0(outcome_type))) %>%
-    dplyr::select(date, paste0(outcome_type)) %>%
-    distinct() %>%
-    rename(type = outcome_type)
-
-  missing_date_type_y <- setdiff(all_date_type_y, obs_date_type_y)
+  all_date_type2 <- expand.grid(date = all_dates,
+                                type = unique(outcome[, type]))
+  obs_date_type2 <- data.table(date = y_ppp$date,
+                               type = y_ppp$type)
+  missing_date_type2 <- setdiff(all_date_type2, obs_date_type2)
 
   ## Combining observed and missing data
-  y_list <- c(y_ppp, rep(list(empty_ppp), nrow(missing_date_type_y)))
-  y_index <- rbind(obs_date_type_y, missing_date_type_y)
-  outcome_types <- pull(unique(outcome[, ..outcome_type]))
+  y_list <- c(y_ppp$V1, rep(list(empty_ppp), nrow(missing_date_type2)))
+  y_index <- rbind(obs_date_type2, missing_date_type2)
+  outcome_types <- unique(outcome[, type])
 
   lapply(1:length(outcome_types),
          function(ii){
@@ -132,7 +118,7 @@ get_hfr <- function(treatment, treatment_type,
 
   for(jj in 1:length(outcome_types)){
     y_hyperframe[, outcome_types[jj]] <-
-      y_list[[jj]][order(subset(y_index, type == outcome_types[[1]])$date)]
+      y_list[[jj]][order(subset(y_index, type == outcome_types[[jj]])$date)]
   }
 
   # Finalizing the hyperframe ----------
