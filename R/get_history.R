aa3b9a0fea5202aa145a51d15eab1b54fc9da615
mmukaigawara <mmukaigawara@gmail.com>
2023-03-08 00:03:25 -0500

    get_history, initial commit

---
+++ R/get_history.R
@@ -0,0 +1,64 @@
+#' Function: get_history
+#'
+#' A function that takes hyperframe that was generated with get_hfr
+#' and generates treatment and outcome histories
+#'
+#' @param tt Point of time of interest
+#' @param Xt A treatment column of a hyperframe
+#' @param Yt An outcome column of a hyperframe
+#' @param lag Lag to aggregate treatment and outcome
+#' @param window Window, should be saved as an owin object
+#' @param x_only Whether to generate treatment history only; by default, FALSE
+
+get_history <- function(tt, Xt, Yt, lag, window, x_only = FALSE) {
+
+  lags <- rep(lag, 2)
+
+  # The variable up_to_time is length 2
+  # corresponding to the starting points to collect history of treatment and outcome
+  up_to_time <- sapply(lags, function(x) max(1, tt - x))
+
+  # Creating history -----
+  Xt_hist <- as.ppp(X = matrix(0, nrow = 0, ncol = 2), W = window)
+  start_time <- tt - 1
+
+  # If the time period is one time point, then this is the time point we use,
+  # otherwise, we superimpose all points.
+  if (start_time == up_to_time[1]) {
+
+    Xt_hist <- Xt[[start_time]]
+
+  } else if (start_time > up_to_time[1]) {
+
+    Xt_hist <- do.call("superimpose", Xt[start_time : up_to_time[1]])
+    Xt_hist$marks <- NULL # Change the shapes of points
+
+  }
+
+  # If x_only = TRUE, then stop here
+  if (x_only) {
+
+    return(list(Xt_hist = Xt_hist))
+
+  } else {
+
+    # Creating outcome history -----
+    Yt_hist <- as.ppp(X = matrix(0, nrow = 0, ncol = 2), W = window)
+    if (tt > 1) {
+
+      Yt_hist <- Yt[[start_time]]
+
+      if (start_time > up_to_time[2]) {
+
+        Yt_hist <- do.call("superimpose", Yt[start_time : up_to_time[2]])
+        Yt_hist$marks <- NULL
+
+      }
+
+    }
+
+    return(list(Xt_hist = Xt_hist, Yt_hist = Yt_hist))
+
+  }
+
+}
