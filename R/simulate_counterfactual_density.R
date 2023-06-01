ecf2b3b87b0de6c348b50ae7ee8925f118d792ab
mmukaigawara <mmukaigawara@gmail.com>
2023-06-01 06:40:35 +0000

    functions for simulations

---
+++ R/simulate_counterfactual_density.R
@@ -0,0 +1,84 @@
+#' Function: simulate_counterfactual_density
+#'
+#' A function that takes a list of power densities
+#' and returns simulated counterfactual densities
+#'
+#' @param expected_number The expected number of observations
+#' @param baseline_density The baseline density (an im object)
+#' @param power_simulation_results The results obtained by simulate_power_density function
+#' @param window An owin object
+#' @param gray_scale By default, FALSE
+
+simulate_counterfactual_density <- function(expected_number,
+                                            baseline_density,
+                                            power_simulation_results,
+                                            window,
+                                            grayscale = FALSE) {
+  
+  power_densities <- power_simulation_results$densities
+  powers <- power_simulation_results$priorities
+  
+  # Obtaining a list of counterfactual densities based on simluations
+  counterfactual_density_list <- lapply(1:length(power_densities),
+                                        function(x) {
+                                          product_power_baseline <- baseline_density * power_densities[[x]]
+                                          counterfactual_density <- product_power_baseline/
+                                            integral(product_power_baseline, W = window) * expected_number
+                                          return(counterfactual_density)
+                                          }
+                                        )
+  
+  # Figure - density
+  sf_density_list <- lapply(1:length(counterfactual_density_list),
+                            function(x) {
+                              sf_density <- stars::st_as_stars(counterfactual_density_list[[x]])
+                              sf_density <- sf::st_as_sf(sf_density) %>% sf::st_set_crs(32650)
+                              return(sf_density)
+                            }
+                            )
+  
+  ## Max value to obtain the zlim
+  max_val <- max(unlist(lapply(1:length(sf_density_list),
+                               function(x) max(sf_density_list[[x]]$v, na.rm = TRUE))))
+  
+  if (grayscale) {
+    
+    plot_list <- lapply(1:length(sf_density_list),
+                        function(a) {
+                          counterfactual_dens <- ggplot() +
+                            ggplot2::geom_sf(data = sf_density_list[[a]], aes(fill = v), col = NA) +
+                            ggplot2::scale_fill_distiller(type = "seq", direction = -1, palette = "Greys") + 
+                            ggplot2::geom_path(data = fortify(as.data.frame(window)), aes(x = x, y = y)) + 
+                            ggthemes::theme_map() +
+                            ggplot2::ggtitle(paste0("alpha = ", powers[a])) +
+                            labs(fill = "Density") +
+                            theme(plot.title = element_text(hjust = 0.5))
+                          
+                          return(counterfactual_dens)
+                        })
+    
+  } else {
+
+    plot_list <- lapply(1:length(sf_density_list),
+                        function(a) {
+                          counterfactual_dens <- ggplot() +
+                            ggplot2::geom_sf(data = sf_density_list[[a]], aes(fill = v), col = NA) +
+                            ggplot2::scale_fill_viridis_c(option = "plasma", limits = c(NA, max_val)) + 
+                            ggplot2::geom_path(data = fortify(as.data.frame(window)), aes(x = x, y = y)) + 
+                            ggthemes::theme_map() +
+                            ggplot2::ggtitle(paste0("alpha = ", powers[a])) +
+                            labs(fill = "Density") +
+                            theme(plot.title = element_text(hjust = 0.5))
+                          
+                          return(counterfactual_dens)
+                        })
+
+  }
+  
+  plot <- ggpubr::ggarrange(plotlist = plot_list, common.legend = TRUE, legend = "bottom")
+  plot <- ggpubr::annotate_figure(plot, top = text_grob("Simulated Counterfactual Densities", face = "bold"))
+  
+  return(list(densities = counterfactual_density_list,
+              plot = plot))
+  
+}
---
+++ R/simulate_power_density.R
@@ -0,0 +1,101 @@
+#' Function: simulate_power_density
+#'
+#' A function that takes the target densities and their priorities
+#' and returns a power density image over a range of parameters
+#'
+#' @param target_densities A list of target densities
+#' @param density_to_manipulate One target density for which we manipulate the value of alpha
+#' @param priorities A vector of priorities for each of target densities
+#' @param priorities_for_manipulation A vector of priority values
+#' @param window An owin object
+#' @param grayscale By default, FALSE
+
+simulate_power_density <- function(target_densities, #This must be a list element
+                                   density_to_manipulate,
+                                   priorities,
+                                   priorities_for_manipulation,
+                                   window,
+                                   grayscale = FALSE) {
+  
+  # Obtaining each target density ^ alpha
+  target_alpha <- lapply(1:length(target_densities), function(x) target_densities[[x]] ^ priorities[x])
+  
+  power_density <- target_alpha[[1]]
+  
+  if (length(target_densities) > 1){
+    
+    for (ii in 2:length(target_densities)){ power_density <- power_density * target_alpha[[ii]] }
+    
+  } 
+
+  power_density_list <- list()
+  
+  for (jj in 1:length(priorities_for_manipulation)){
+    temp <- power_density * density_to_manipulate ^ priorities_for_manipulation[jj]
+    temp <- temp/integral(temp, domain = window)
+    power_density_list[[jj]] <- temp
+  }
+  
+  power_density_list <- lapply(1:length(priorities_for_manipulation),
+                               function(x) {
+                                 temp <- power_density * density_to_manipulate ^ priorities_for_manipulation[x]
+                                 temp <- temp/integral(temp, domain = window)
+                                 return(temp)
+                               }
+                               )
+  
+  # Figure - density
+  sf_density_list <- lapply(1:length(power_density_list),
+                            function(x) {
+                              sf_density <- stars::st_as_stars(power_density_list[[x]])
+                              sf_density <- sf::st_as_sf(sf_density) %>% sf::st_set_crs(32650)
+                              return(sf_density)
+                            }
+                            )
+
+  ## Max value to obtain the zlim
+  max_val <- max(unlist(lapply(1:length(sf_density_list),
+                               function(x) max(sf_density_list[[x]]$v, na.rm = TRUE))))
+  
+  if (grayscale) {
+    
+    plot_list <- lapply(1:length(sf_density_list),
+                        function(a) {
+                          power_dens <- ggplot() +
+                            ggplot2::geom_sf(data = sf_density_list[[a]], aes(fill = v), col = NA) +
+                            ggplot2::scale_fill_distiller(type = "seq", direction = -1, palette = "Greys") + 
+                            ggplot2::geom_path(data = fortify(as.data.frame(window)), aes(x = x, y = y)) + 
+                            ggthemes::theme_map() +
+                            ggplot2::ggtitle(paste0("alpha = ", priorities_for_manipulation[a])) +
+                            labs(fill = "Density") +
+                            theme(plot.title = element_text(hjust = 0.5))
+                          
+                          return(power_dens)
+                        })
+    
+  } else {
+    
+    plot_list <- lapply(1:length(sf_density_list),
+                        function(a) {
+                          power_dens <- ggplot() +
+                            ggplot2::geom_sf(data = sf_density_list[[a]], aes(fill = v), col = NA) +
+                            ggplot2::scale_fill_viridis_c(option = "plasma", limits = c(NA, max_val)) + 
+                            ggplot2::geom_path(data = fortify(as.data.frame(window)), aes(x = x, y = y)) + 
+                            ggthemes::theme_map() +
+                            ggplot2::ggtitle(paste0("alpha = ", priorities_for_manipulation[a])) +
+                            labs(fill = "Density") +
+                            theme(plot.title = element_text(hjust = 0.5))
+                          
+                          return(power_dens)
+                        })
+    
+  }
+  
+  plot <- ggpubr::ggarrange(plotlist = plot_list, common.legend = TRUE, legend = "bottom")
+  plot <- ggpubr::annotate_figure(plot, top = text_grob("Simulated Power Densities", face = "bold"))
+  
+  return(list(densities = power_density_list,
+              plot = plot,
+              priorities = priorities_for_manipulation))
+
+}
