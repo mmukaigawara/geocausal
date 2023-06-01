962babadb2e50a13dc30f3c75369fdb1556a9fc2
mmukaigawara <mmukaigawara@gmail.com>
2023-06-01 16:41:17 +0000

    get_window function

---
+++ R/get_window.R
@@ -0,0 +1,42 @@
+#' Function: get_window
+#'
+#' A function that takes a directory that hosts shp file
+#' and returns an owin object
+#'
+#' @param load_path A path to the shp file (note: a folder)
+
+get_window <- function(load_path) {
+  
+  # Reading the data
+  temp <- sf::st_read(file.path(load_path))
+  
+  # Converting the data
+  temp <- sf::st_union(st_as_sf(temp))
+  temp_matrix <- sf::st_coordinates(temp)
+  
+  # Checking whether data is clockwise
+  clockwise <- function(xx) {
+    
+    x.coords <- c(xx[[1]], xx[[1]][1])
+    y.coords <- c(xx[[2]], xx[[2]][1])
+    
+    double.area <- sum(sapply(2:length(x.coords), function(ii) {
+      (x.coords[ii] - x.coords[ii-1])*(y.coords[ii] + y.coords[ii-1])
+    }))
+    
+    double.area > 0
+  }
+  
+  clockwise_check <- clockwise(data.frame(x = temp_matrix[, 1],
+                                          y = temp_matrix[, 2]))
+  ??owin
+  # Generating a window
+  if (clockwise) {
+    temp_window <- spatstat.geom::owin(poly = data.frame(x = rev(temp_matrix[, 1]), y = rev(temp_matrix[, 2])))
+  } else {
+    temp_window <- spatstat.geom::owin(poly = data.frame(x = temp_matrix[, 1], y = temp_matrix[, 2]))
+  }
+  
+  return(temp_window)
+  
+}
