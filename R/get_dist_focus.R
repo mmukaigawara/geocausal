1a858497d31641b38fc67d2ec43999d485bb6a09
mmukaigawara <mmukaigawara@gmail.com>
2023-06-22 20:36:00 +0000

    omitted old function

--- R/get_dist_focus.R
+++ R/get_dist_focus.R
@@ -138,22 +138,3 @@ get_dist_focus <- function(window, longitude, latitude, resolution,
   return(list(distance_im = distance_im, plot = gg))
   
 }
-
-# Old function
-#get_dist_focus <- function(focus_locations,
-#                           window) {
-  
-  # Convert to ppp
-#  focus_locations_ppp <- spatstat.geom::as.ppp(X = focus_locations, W = window)
-  
-  # Get a distance map
-#  focus_locations_distance <- spatstat.geom::distmap(focus_locations_ppp, dimyx = 256)
-#  spatstat.geom::Window(focus_locations_distance) <- window #Add window
-  
-  # Take the exponential of -distance and divide it by the integral
-#  neg_exp_distance <- exp(-focus_locations_distance)
-#  distance_from_focus <- neg_exp_distance/integral(neg_exp_distance)
-  
-#  return(distance_from_focus)
-  
-#}
