a95ef819fbcf8d428bdc20ae5f64ca284449f32f
mmukaigawara <mmukaigawara@gmail.com>
2023-06-23 13:48:47 +0000

    function for causal contrast

---
+++ R/get_causal_contrasts.R
@@ -0,0 +1,95 @@
+#' Function: get_causal_contrasts
+#'
+#' A function that returns a figure with causal contrasts
+#'
+#' @param counterfactual_density_1 A counterfactual density (same for _2)
+ 
+get_causal_contrasts <- function(estimates_scenario_1, #Contrast = scenario 2 - scenario 1
+                                 estimates_scenario_2,
+                                 grayscale, expectation_use_raw) {
+  
+  
+  # Create a dataframe for causal contrast
+  causal_contrast <- estimates_scenario_2$average_expected_events_quantiles - 
+    estimates_scenario_1$average_expected_events_quantiles
+  
+  distance_quantiles <- estimates_scenario_1$distance_quantiles
+  
+  result_data <- data.frame(expectation = causal_contrast,
+                            distance = distance_quantiles)
+  
+  # Plot for distance-based expectations
+  
+  x_label_text <- paste0("Distance from the focus (", distance_map_unit, ")")
+  
+  if(grayscale) {
+    
+    if (expectation_use_raw) {
+      
+      expectation_plot <- ggplot(result_data) +
+        ggplot2::geom_line(aes(x = distance, y = expectation)) +
+        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+        theme_bw() +
+        labs(x = x_label_text, 
+             y = "The expected outcome events\ncovered by the area",
+             color = latex2exp::TeX("$\\alpha_{focus}$")) +
+        ggplot2::scale_color_brewer(palette = "Greys") +
+        theme(plot.margin = margin(0.1, 0.1, 1, 0.1, "cm")) 
+      
+    } else {
+      
+      expectation_plot <- ggplot(result_data) +
+        ggplot2::geom_line(aes(x = distance, y = expectation)) +
+        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+        theme_bw() +
+        labs(x = x_label_text, 
+             y = "The proportion of\nexpected outcome events\ncovered by the area",
+             color = latex2exp::TeX("$\\alpha_{focus}$")) +
+        ggplot2::scale_color_brewer(palette = "Greys") +
+        ylim(0, 1) + theme(plot.margin = margin(0.1, 0.1, 1, 0.1, "cm")) 
+      
+    }
+    
+  } else {
+    
+    if (expectation_use_raw) {
+      
+      expectation_plot <- ggplot(result_data) +
+        ggplot2::geom_line(aes(x = distance, y = expectation)) +
+        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+        theme_bw() +
+        labs(x = x_label_text, 
+             y = "The expected outcome events\ncovered by the area",
+             color = latex2exp::TeX("$\\alpha_{focus}$")) +
+        ggplot2::scale_color_brewer(palette = "Greys") +
+        theme(plot.margin = margin(0.1, 0.1, 1, 0.1, "cm")) 
+      
+    } else {
+      
+      expectation_plot <- ggplot(result_data) +
+        ggplot2::geom_line(aes(x = distance, y = expectation)) +
+        ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+        theme_bw() +
+        labs(x = x_label_text, 
+             y = "The proportion of\nexpected outcome events\ncovered by the area",
+             color = latex2exp::TeX("$\\alpha_{focus}$")) +
+        ggplot2::scale_color_brewer(palette = "Greys") +
+        ylim(0, 1) + theme(plot.margin = margin(0.1, 0.1, 1, 0.1, "cm")) 
+      
+    }
+    
+  }
+  
+  # Color and plot
+  window_plot <- estimates_scenario_1$window_plot
+  
+  entire_plot <- ggpubr::ggarrange(expectation_plot, window_plot, nrow = 2, heights = c(0.7, 0.3))
+  titletext <- "Causal Effects Per Time Period:\nThe Expected Number of Outcome Events\nComparing Two Counterfactual Scenarios"
+  entire_plot <- ggpubr::annotate_figure(entire_plot, top = ggpubr::text_grob(titletext, face = "bold"))
+  
+  return(list(plot = entire_plot,
+              expectation_plot = expectation_plot,
+              df = result_data)
+         )
+  
+}
