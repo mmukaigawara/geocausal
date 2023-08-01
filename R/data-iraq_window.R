86ebed7ce7d2bf43fdd0eefaa92f6b4b04744b54
mmukaigawara <mmukaigawara@gmail.com>
2023-08-01 07:22:12 +0000

    data

--- R/data-iraq_window.R
+++ R/data-iraq_window.R
@@ -3,3 +3,10 @@
 #' An owin object of Iraq
 #'
 #' @format A polygonal object (list)
+#' \describe{
+#' \item{type}{"polygonal"}
+#' \item{xrange}{Range (longitude)}
+#' \item{yrange}{Range (latitude)}
+#' \item{bdry}{Boundaries}
+#' \item{units}{Units}
+#' }
