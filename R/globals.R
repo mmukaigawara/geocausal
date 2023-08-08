df1b700589b7a04b176ceb6b00d87ba44a1347e6
Mitsuru Mukaigawara <mmukaigawara@gmail.com>
2023-08-08 21:47:30 +0900

    global variable .

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 utils::globalVariables(c("x", "y", "z", "value", ".data", "count",
                          "distance", "expectation", "longitude", "latitude",
                          "time", "time_period", "type", "subtype",
-                         "alpha", "var_upper_bound"))
+                         "alpha", "var_upper_bound", "."))
