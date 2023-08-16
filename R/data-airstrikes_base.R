38f83edff01bb5232fc91083a3b474df614eb5af
mmukaigawara <mmukaigawara@gmail.com>
2023-08-16 09:25:47 +0000

    update

---
+++ R/data-airstrikes_base.R
@@ -0,0 +1,12 @@
+#' airstrikes_base
+#'
+#' A subset of airstrikes data in Iraq (a subset of airstrikes in January 2006) that can be used to construct baseline densities
+#'
+#' @format A tibble with 182 rows and 2 variables:
+#' \describe{
+#' \item{longitude}{Longitudes (decimal)}
+#' \item{latitude}{Latitudees (decimal)}
+#' }
+#' @examples
+#' airstrikes_base
+"airstrikes_base"
