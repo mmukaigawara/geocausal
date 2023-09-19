90046d85e270d7f80d3496a01069d59f7ae760c3
Mitsuru Mukaigawara <mmukaigawara@gmail.com>
2023-09-19 00:54:55 +0000

    update get_base_dens()

--- R/get_base_dens.R
+++ R/get_base_dens.R
@@ -1,38 +1,77 @@
 #' Get baseline densities
 #'
 #' @description `get_base_dens()` takes a dataframe and 
-#' returns the baseline densities using Scott's rule of thumb.
+#' returns the baseline densities using Scott's rule of thumb (out-of-sample data)
+#' or fitting an inhomogeneous Poisson model (in-sample data) by regressing
+#' the in-sample data on time-invariant covariates.
 #'
-#' @param data dataframe
-#' @param coordinates vector of column names of longitudes and latitudes (in this order)
 #' @param window owin object
+#' @param option "in" (using in-sample data) or "out" (using out-of-sample data)
 #' @param grayscale logical. `grayscale` specifies whether to convert plot to grayscale (by default, FALSE).
-#' 
+#' @param ndim the number of dimensions of grid cells (ndim^2). By default, ndim = 256.
+#' @param out_data dataframe (if using out-of-sample data)
+#' @param out_coordinates vector of column names of longitudes and latitudes (in this order) (if using in-sample data)
+#' @param hfr hyperframe (if using in-sample data)
+#' @param dep_var the name of the dependent variable (if using in-sample data)
+#' @param indep_var the names of time-invariant independent varaibles (if using in-sample data)
+#' @param ratio for random sampling of data (if using in-sample data)
+
+#'
 #' @returns list of the following:
 #'     * `density`: im object of baseline density
 #'     * `density_plot`: ggplot object of baseline density
-#'     * `point_plot`: ggplot object of raw data
 #'
 #' @examples
-#' get_base_dens(data = airstrikes_base, 
-#'               coordinates = c("longitude", "latitude"),
+#' get_base_dens(option = "out",
+#'               out_data = airstrikes_base, 
+#'               out_coordinates = c("longitude", "latitude"),
 #'               window = iraq_window,
-#'               grayscale = FALSE)
+#'               grayscale = FALSE,
+#'               ndim = 256)
 
-get_base_dens <- function(data, 
-                          coordinates = c("longitude", "latitude"),
-                          window,
-                          grayscale = FALSE){
+get_base_dens <- function(window,
+                          option,
+                          grayscale = FALSE,
+                          ndim = 256,
+                          out_data,
+                          out_coordinates = c("longitude", "latitude"),
+                          hfr,
+                          dep_var,
+                          indep_var,
+                          ratio
+){
   
-  # Convert data to ppp
-  coordinates_data <- data[, coordinates]
-  baseline_ppp <- spatstat.geom::as.ppp(coordinates_data, W = window)
+  # Option 1. Using out-of-sample data
   
-  # Apply Scott's rule of thumb
-  scott_bandwidth <- spatstat.explore::bw.scott(baseline_ppp)
-  
-  baseline_density <- stats::density(baseline_ppp, scott_bandwidth, dimyx = 256) #Kernel density estimation
-  baseline_density <- baseline_density / spatstat.geom::integral(baseline_density) #Divide by integral of the density
+  if (option == "out") {
+    
+    message("Using out-of-sample data to obtain the baseline density")
+    
+    # Convert out-of-sample data to ppp
+    coordinates_data <- out_data[, out_coordinates]
+    baseline_ppp <- spatstat.geom::as.ppp(coordinates_data, W = window)
+    
+    # Apply Scott's rule of thumb
+    scott_bandwidth <- spatstat.explore::bw.scott(baseline_ppp)
+    
+    baseline_density <- stats::density(baseline_ppp, scott_bandwidth, dimyx = ndim) #Kernel density estimation
+    baseline_density <- baseline_density / spatstat.geom::integral(baseline_density) #Divide by integral of the density
+    
+  } else if (option == "in") { # Option 2. Using in-sample data
+    
+    message("Using in-sample data to obtain the baseline density")
+    
+    num_obs <- round(ratio*nrow(dat_hfr_nga), digits = 0) #The number of obs to use
+    ids <- sample(c(rep(1, num_obs), rep(0, nrow(dat_hfr_nga) - num_obs)))
+    hfr <- hfr[which(ids == 1), ]
+    
+    text_form <- paste0(dep_var, " ~ ", paste(indep_var, collapse = " + "))
+    
+    mod <- spatstat.model::mppm(as.formula(text_form), data = hfr, nd = ndim) #Use mppm
+    pred <- spatstat.model::predict.mppm(mod, ngrid = ndim, type = "cif")[1, ] #Pick the first one (all identical)
+    baseline_density <- pred[["cif"]] / spatstat.geom::integral(pred[["cif"]])
+    
+  }
   
   # ggplot figure - density
   
@@ -71,17 +110,6 @@ get_base_dens <- function(data,
     
   }
   
-  # ggplot figure - points
+  return(list(density = baseline_density, density_plot = baseline_dens))
   
-  points <- as.data.frame(baseline_ppp)
-  
-  baseline_ppp <- ggplot() +
-    ggplot2::geom_point(data = points, aes(x = x, y = y), size = 0.5, col = "black") +
-    ggplot2::geom_path(data = as.data.frame(window), aes(x = x, y = y), color = "black") + 
-    ggthemes::theme_map() +
-    ggplot2::ggtitle("Observed Treatment Events") +
-    theme(plot.title = element_text(hjust = 0.5, face = "bold"))
-  
-  return(list(density = baseline_density, density_plot = baseline_dens, point_plot = baseline_ppp))
-
 }
