cc2f11ecea2db97811c62cc22ef8189e53383d99
Mitsuru Mukaigawara <mmukaigawara@gmail.com>
2023-12-27 13:49:10 +0000

    class dens im

--- R/plot.im.R
+++ R/plot.im.R
@@ -7,20 +7,20 @@
 #' @param grayscale whether to use grayscale. By default, FALSE.
 #' 
 #' @export
-plot.dens <- function(x, ..., main = "Density", window, grayscale = "FALSE") {
-  
+plot.im <- function(x, ...,  main = "Distance", window, grayscale = "FALSE") {
+
   ## Convert the density image to a data frame
   pd_df <- as.data.frame(x)
-  
+
   ## Pivot the data frame to a long format
   pd_df_long <- tidyr::pivot_longer(pd_df, cols = starts_with("V"), names_to = "variable", values_to = "value")
-  
-  ## Extract owin - not working well for densities
+
+  ## Extract owin
   #window <- spatstat.geom::Window(x)
-  
+
   ## Plot the image using ggplot2
-  if(grayscale) {
-    
+  if (grayscale) {
+  
     plot_dens <- ggplot() +
       ggplot2::geom_tile(data = pd_df_long, aes(x = x, y = y, fill = value)) +
       ggplot2::scale_fill_distiller(type = "seq", direction = -1, palette = "Greys") +
@@ -30,9 +30,9 @@ plot.dens <- function(x, ..., main = "Density", window, grayscale = "FALSE") {
       labs(fill = "Density") +
       theme(plot.title = element_text(hjust = 0.5, face = "bold"),
             plot.subtitle = element_text(hjust = 0.5))
-    
+  
   } else {
-    
+  
     plot_dens <- ggplot() +
       ggplot2::geom_tile(data = pd_df_long, aes(x = x, y = y, fill = value)) +
       ggplot2::scale_fill_viridis_c(option = "plasma") + 
@@ -44,7 +44,7 @@ plot.dens <- function(x, ..., main = "Density", window, grayscale = "FALSE") {
             plot.subtitle = element_text(hjust = 0.5))
     
   }
-  
+
   return(plot_dens)
-  
+
 }
