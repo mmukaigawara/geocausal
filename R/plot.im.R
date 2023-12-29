bf234d395cc34ac40a8e6fe18d665e680b033f77
mmukaigawara <mmukaigawara@gmail.com>
2023-12-29 10:33:11 +0900

    lim

--- R/plot.im.R
+++ R/plot.im.R
@@ -1,5 +1,5 @@
 #' Plot im
-#' 
+#'
 #' @param x input
 #' @param ... arguments passed on to the function
 #' @param main title
@@ -7,7 +7,7 @@
 #' @param scalename the name of the scale (for images only)
 #' @param lim limits of the scale. By default, NA. To set limits manually, provide a vector or max and min
 #' @param grayscale whether to use grayscale. By default, FALSE.
-#' 
+#'
 #' @export
 plot.im <- function(x, ...,  main = "Image object", window, scalename = "Density", grayscale = "FALSE", lim = NA) {
 
@@ -23,42 +23,42 @@ plot.im <- function(x, ...,  main = "Image object", window, scalename = "Density
   ## Plot the image using ggplot2
   plot_dens <- ggplot() +
     ggplot2::geom_tile(data = pd_df_long, aes(x = x, y = y, fill = value)) +
-    ggplot2::geom_path(data = as.data.frame(window), aes(x = x, y = y), color = "white") + 
+    ggplot2::geom_path(data = as.data.frame(window), aes(x = x, y = y), color = "white") +
     ggthemes::theme_map() +
     ggplot2::ggtitle(main) +
     labs(fill = scalename) +
     theme(plot.title = element_text(hjust = 0.5, face = "bold"),
           plot.subtitle = element_text(hjust = 0.5))
-  
+
   if (grayscale) {
-    
-    if (is.na(lim)) {
-      
+
+    if (is.na(lim)[1]) {
+
       plot_dens <- plot_dens +
         ggplot2::scale_fill_distiller(type = "seq", direction = -1, palette = "Greys")
-      
+
     } else {
 
       plot_dens <- plot_dens +
         ggplot2::scale_fill_distiller(type = "seq", direction = -1, palette = "Greys", limits = lim)
-      
+
     }
-    
-    
+
+
   } else {
-    
+
     if (is.na(lim)) {
-      
+
       plot_dens <- plot_dens +
         ggplot2::scale_fill_viridis_c(option = "plasma")
-      
+
     } else {
-      
-      plot_dens <- plot_dens + 
+
+      plot_dens <- plot_dens +
         ggplot2::scale_fill_viridis_c(option = "plasma", limits = lim)
-      
+
     }
-    
+
   }
 
   return(plot_dens)
