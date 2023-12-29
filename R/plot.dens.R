5031c0f44cc0e3470cf30aac36e1a15061956c08
mmukaigawara <mmukaigawara@gmail.com>
2023-12-29 14:40:55 +0900

    tidyterra

--- R/plot.dens.R
+++ R/plot.dens.R
@@ -3,27 +3,27 @@
 #' @param x input
 #' @param ... arguments passed on to the function
 #' @param main title
-#' @param window owin object
 #' @param scalename the name of the scale (for images only)
 #' @param lim limits of the scale. By default, NA. To set limits manually, provide a vector or max and min
 #' @param grayscale whether to use grayscale. By default, FALSE.
 #'
 #' @export
-plot.dens <- function(x, ...,  main = "Image object", window, scalename = "Density", grayscale = "FALSE", lim = NA) {
+plot.dens <- function(x, ...,  main = "Image object", scalename = "Density", grayscale = "FALSE", lim = NA) {
 
   ## Convert the density image to a data frame
-  pd_df <- as.data.frame(x)
+  #pd_df <- as.data.frame(x)
 
   ## Pivot the data frame to a long format
-  pd_df_long <- tidyr::pivot_longer(pd_df, cols = starts_with("V"), names_to = "variable", values_to = "value")
+  #pd_df_long <- tidyr::pivot_longer(pd_df, cols = starts_with("V"), names_to = "variable", values_to = "value")
 
   ## Extract owin
   #window <- spatstat.geom::Window(x)
 
   ## Plot the image using ggplot2
   plot_dens <- ggplot() +
-    ggplot2::geom_tile(data = pd_df_long, aes(x = x, y = y, fill = value)) +
-    ggplot2::geom_path(data = as.data.frame(window), aes(x = x, y = y), color = "white") +
+    tidyterra::geom_spatraster(data = terra::rast(x)) +
+    #ggplot2::geom_tile(data = pd_df_long, aes(x = x, y = y, fill = value)) +
+    #ggplot2::geom_path(data = as.data.frame(window), aes(x = x, y = y), color = "white") +
     ggthemes::theme_map() +
     ggplot2::ggtitle(main) +
     labs(fill = scalename) +
