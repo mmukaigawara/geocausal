2a16c5747558edc0d4ecc8e0d8e26727567dabd3
Mitsuru Mukaigawara <mmukaigawara@gmail.com>
2023-12-29 00:01:58 +0000

    lim

--- R/plot.est.R
+++ R/plot.est.R
@@ -2,9 +2,10 @@
 #' 
 #' @param x input
 #' @param ... arguments passed on to the function
+#' @param lim limits of the scale. By default, NA. To set limits manually, provide a vector or max and min
 #' 
 #' @export
-plot.est <- function(x, ...) {
+plot.est <- function(x, ..., lim = NA) {
   
   estimates <- x
   
@@ -22,14 +23,28 @@ plot.est <- function(x, ...) {
     ggplot2::coord_quickmap() +
     ggplot2::geom_polygon(data = polygon_df, aes(x = longitude, y = latitude), 
                           fill = NA, color = "black", size = 0.2) +
-    ggplot2::scale_fill_gradientn(
-      colors = c("steelblue1", "white", "violetred"),
-      limits = c(-max(abs(df_surf$value)), max(abs(df_surf$value)))
-    ) +
     ggthemes::theme_map() +
     ggplot2::ggtitle("Differences in average weighted surfaces") + labs(fill = "Density") +
     theme(plot.title = element_text(hjust = 0.5, face = "bold"))
   
+  if (is.na(lim)) {
+    
+    surf_plot <- surf_plot +
+      ggplot2::scale_fill_gradientn(
+        colors = c("steelblue1", "white", "violetred"),
+        limits = c(-max(abs(df_surf$value)), max(abs(df_surf$value)))
+      ) 
+      
+  } else {
+    
+    surf_plot <- surf_plot +
+      ggplot2::scale_fill_gradientn(
+        colors = c("steelblue1", "white", "violetred"),
+        limits = lim
+      ) 
+    
+  }
+
   #2. Visualization 2: Causal effects -----
   
   ## Prepare data
@@ -49,12 +64,10 @@ plot.est <- function(x, ...) {
     ggplot2::geom_linerange(aes(ymin = lower_95, ymax = upper_95)) +
     ggplot2::geom_linerange(aes(ymin = lower_90, ymax = upper_90), lwd = 1.1) +
     theme_bw() +
-    labs(title = "Causal effects", x = "Windows", y = "Expected outcomes per time period",
-         color = "Effect types", fill = "Effect types") +
-    theme_bw() +
     ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-    labs(y = "Expected outcome events",
-         title = "Causal effects per time period") +
+    labs(y = "Expected outcome events", x = "Windows",
+         title = "Causal effects per time period",
+         color = "Effect types", fill = "Effect types") +
     theme(plot.title = element_text(hjust = 0.5, face = "bold"),
           plot.subtitle = element_text(hjust = 0.5),
           panel.grid.major = element_blank(), panel.grid.minor = element_blank())
