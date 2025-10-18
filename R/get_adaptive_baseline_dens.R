989edaa1f0c59c72fa9f1bc107aa1af53894f032
lingxiaozhou <35700851+lingxiaozhou@users.noreply.github.com>
2025-10-18 17:17:09 -0400

    minor

--- R/get_adaptive_baseline_dens.R
+++ R/get_adaptive_baseline_dens.R
@@ -1,3 +1,34 @@
+#' Generate adaptive intervention densities based on historical data
+#'
+#' @description
+#' `get_adaptive_baseline_dens()` takes a hyperframe for historical data and returns fitted densities for the current data.
+#' The output is used as counterfactural densities.
+#'
+#' @param hfr_hist hyperframe for historical data
+#' @param hfr_current hyperframe for current data
+#' @param dep_var The name of the dependent variable
+#' Since we need to obtain the counterfactural density of treatment events,
+#' `dep_var` should be the name of the treatment variable.
+#' @param indep_var vector of names of independent variables (covariates)
+#' @param ngrid the number of grid cells that is used to generate observed densities.
+#' By default = 100. Notice that as you increase `ngrid`, the process gets computationally demanding.
+#' @param window owin object
+#'
+#' @returns list of the following:
+#'      * `indep_var`: independent variables
+#'      * `coef`: coefficients
+#'      * `intens_grid_cells`: im object of observed densities for each time period
+#'      * `estimated_counts`: the number of events that is estimated by the poisson point process model for each time period
+#'      * `sum_log_intens`: the sum of log intensities for each time period
+#'      * `actual_counts`: the number of events (actual counts)
+#'
+#' @details `get_obs_dens()` assumes the poisson point process model and
+#' calculates observed densities for each time period. It depends on `spatstat.model::mppm()`.
+#' Users should note that the coefficients in the output are not directly interpretable,
+#' since they are the coefficients inside the exponential of the poisson model.
+
+
+
 get_adaptive_baseline_dens <- function(hfr_hist, hfr_current, dep_var, indep_var,
                               ngrid = 100, window) {
 
@@ -16,7 +47,7 @@ get_adaptive_baseline_dens <- function(hfr_hist, hfr_current, dep_var, indep_var
   
   coefficients <- as.numeric(spatstat.model::summary.mppm(mod)$coef)
   
-  # ---- 2) Predict CIF on the *current* hyperframe ----
+  # ---- 2) Predict CIF on the current hyperframe ----
   # Returns a list of im objects, one per row of hfr_current
   message("Predicting CIF on *current* data using the baseline model...\n")
   cif_current <- spatstat.model::predict.mppm(
@@ -31,7 +62,7 @@ get_adaptive_baseline_dens <- function(hfr_hist, hfr_current, dep_var, indep_var
     FUN.VALUE = numeric(1)
   )
   
-  # ---- 4) Sum of log intensity at *current* observed points ----
+  # ---- 4) Sum of log intensity at current observed points ----
   # Evaluate each predicted CIF image at the actual current point pattern
   # and sum log(lambda) safely (0 if no points that period).
   message("Computing sum of log intensity at observed points (current data)...\n")
@@ -46,18 +77,17 @@ get_adaptive_baseline_dens <- function(hfr_hist, hfr_current, dep_var, indep_var
     }
   )
   
-  # ---- 5) Actual counts for the *current* periods ----
+  # ---- 5) Actual counts for the current periods ----
   actual_counts <- unlist(purrr::map(hfr_current[[dep_var]], function(x) x$n))
   
-  # ---- 6) Return object similar to your obs object ----
+  # ---- 6) Return object ----
   out <- list(
     indep_var          = indep_var,
     coef               = coefficients,
     intens_grid_cells  = cif_current,      # predicted CIF images on current data
     estimated_counts   = estimated_counts, # integrated CIF over window
     sum_log_intens     = sum_log_intensity,
-    actual_counts      = actual_counts,
-    model              = mod               # keep the fitted baseline model if needed
+    actual_counts      = actual_counts
   )
   class(out) <- c("obs", "list")
   return(out)
--- R/get_cf_dens_adaptive.R
+++ R/get_cf_dens_adaptive.R
@@ -1,3 +1,20 @@
+#' Get counterfactual densities
+#'
+#' @description `get_cf_dens_adaptive` takes the scale_factor, baseline density to construct counterfactural intensities .
+#'
+#' @param baseline_den baseline density (obs object obtained from `get_adaptive_baseline_dens`)
+#' @param scale_factor a positive number that scales the baseline intensity
+#'
+#' @returns an list of the following:
+#'      * `intens_grid_cells`: im object of counterfactural intensities for each time period
+#'      * `estimated_counts`: the number of events that is estimated by the poisson point process model for each time period
+#'      * `sum_log_intens`: the sum of log intensities for each time period
+
+
+
+
+
+
 get_cf_dens_adaptive <- function(baseline_den, scale_factor) {
   if (!all(c("intens_grid_cells", "estimated_counts", "sum_log_intens", "actual_counts") %in% names(baseline_den))) {
     stop("Input baseline_denect must include intens_grid_cells, estimated_counts, sum_log_intens, and actual_counts.")
@@ -15,14 +32,11 @@ get_cf_dens_adaptive <- function(baseline_den, scale_factor) {
   #    Each term increases by n_t * log(scale_factor)
   scaled_sum_log <- baseline_den$sum_log_intens + baseline_den$actual_counts * log(scale_factor)
   
-  # 4. Return a new list (drop model)
+  # 4. Return a list 
   out_scaled <- list(
-    indep_var         = baseline_den$indep_var,
-    coef              = baseline_den$coef,
     intens_grid_cells = scaled_cif,
     estimated_counts  = scaled_est_counts,
-    sum_log_intens    = scaled_sum_log,
-    actual_counts     = baseline_den$actual_counts
+    sum_log_intens    = scaled_sum_log
   )
 
   return(out_scaled)
