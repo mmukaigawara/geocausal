9b7fd8d213b2dec9dfc25e54596800a384a27f69
mmukaigawara <mmukaigawara@gmail.com>
2025-12-25 17:23:44 +0900

    get_window

--- R/get_window.R
+++ R/get_window.R
@@ -4,16 +4,35 @@
 #' and returns an owin object.
 #'
 #' @param load_path path to the shp file
+#' @param target_crs target CRS (if users want to specify it; as an EPSG code)
 #'
 #' @returns owin object
 
-get_window <- function(load_path) {
+get_window <- function(load_path, target_crs = NULL) {
 
-  # Reading the data
-  temp <- sf::st_read(file.path(load_path))
+  # 1. Reading the data
+  temp <- sf::st_read(load_path, quiet = TRUE)
 
-  # Converting the data
-  #temp <- sf::st_union(sf::st_as_sf(temp)) #union only required if we need to combine shapefiles at lower admin levels
+  # 2. Handling Projection
+  if (!is.null(target_crs)) {
+
+    temp <- sf::st_transform(temp, crs = target_crs)
+
+  } else if (sf::st_is_longlat(temp)) {
+
+    suggested <- crsuggest::suggest_crs(temp, units = "m")
+
+    if (nrow(suggested) > 0) {
+      auto_crs <- as.numeric(suggested$crs_code[1])
+      message(paste0("Projecting to suggested Cartesian CRS: EPSG:", auto_crs))
+      temp <- sf::st_transform(temp, crs = auto_crs)
+    } else {
+      stop("No suggested Cartesian CRS was identified. Please provide a 'target_crs' for projection.")
+    }
+  }
+
+  # 3. Converting the data
+  #temp_union <- sf::st_union(temp)
   temp_matrix <- sf::st_coordinates(temp)
 
   # Checking whether data is clockwise
@@ -29,14 +48,17 @@ get_window <- function(load_path) {
     double.area > 0
   }
 
+  # temp_matrix[, 1] and [, 2] are now in meters/Cartesian units
   clockwise_check <- clockwise(data.frame(x = temp_matrix[, 1],
                                           y = temp_matrix[, 2]))
 
   # Generating a window
   if (clockwise_check) {
-    temp_window <- spatstat.geom::owin(poly = data.frame(x = rev(temp_matrix[, 1]), y = rev(temp_matrix[, 2])))
+    temp_window <- spatstat.geom::owin(poly = data.frame(x = rev(temp_matrix[, 1]),
+                                                         y = rev(temp_matrix[, 2])))
   } else {
-    temp_window <- spatstat.geom::owin(poly = data.frame(x = temp_matrix[, 1], y = temp_matrix[, 2]))
+    temp_window <- spatstat.geom::owin(poly = data.frame(x = temp_matrix[, 1],
+                                                         y = temp_matrix[, 2]))
   }
 
   return(temp_window)
