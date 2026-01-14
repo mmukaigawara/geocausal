b00a7415b24491fb57ae817297c72a3131771af7
Lingxiao Zhou <zhou.l@ufl.edu>
2026-01-14 14:43:36 -0500

    update function for flexible dimensions

--- R/get_em_vec.R
+++ R/get_em_vec.R
@@ -4,24 +4,35 @@
 #' `get_em_vec()` get the vector form of a column of a hyperframe that summarizes the effect modifier data in heterogeneity analysis
 #'
 #' @param em column of a hyperframe that summarizes effect modifier data. In the form of `hyperframe$column`.
+#' @param outcome_pixel_count
+#'   A list of integer-valued pixel images giving outcome event counts per spatial pixel,
+#'   typically obtained from `pixel_count_ppp()`.
 #' @param time_after whether to include one unit time difference between treatment and outcome. By default = TRUE
 #' @param lag integer that specifies lags to calculate causal estimates
 #' @param entire_window owin object (the entire region of interest). If given, then the values outside the region will be set to `NA`.
-#' @param ngrid a number or a vector of two numbers that specify the dimension of pixels. If NULL, the pixel dimension of the original
-#' images will not be changed
-#' 
+#'
 #' @details The function `get_em_vec()` get the vector form of the effect modifier in the heterogeneity analysis. It is useful
 #' if you want to construct the variance matrix `E_mat` that is passed to the function `get_cate()`
-
-
-get_em_vec <- function(em,time_after = TRUE,lag,entire_window = NULL,ngrid=NULL){
-  if(length(ngrid)==1){
-    ngrid <- rep(ngrid,2)
-  }
-  l <- length(em)
-  res <- imls_to_arr(em,start=1,end=l-time_after-lag+1,entire_window,ngrid = ngrid)
-  res <- c(res)
+get_em_vec <- function(em,
+                       outcome_pixel_count = NULL,
+                       time_after = TRUE,
+                       entire_window = NULL,
+                       lag) {
   
+  
+  if (is.null(outcome_pixel_count)) {
+    stop("`outcome_pixel_count` must be provided to determine the pixel grid.")
+  }
+
+  l <- length(em)
+  dimyx <- dim(outcome_pixel_count[[1]])
+
+  res <- imls_to_arr(em,
+                     start = 1,
+                     end = l - time_after - lag + 1,
+                     entire_window = entire_window,
+                     dimyx = dimyx)
+  
+  res <- c(res)
   return(res)
 }
-
--- R/imls_to_arr.R
+++ R/imls_to_arr.R
@@ -6,47 +6,45 @@
 #' @param imls a list of im objects (imlist)
 #' @param start the index of the first im to be converted. Default is 1.
 #' @param end the index of the last im to be converted. If not provided, then it will be set to the length of the list.
-#' @param entire_window a owin object. If given, then the values outside the region will be set to `NA`
-#' @param ngrid  an optional arugument that takes one integer or  vector of two integers specifying the dimensions of the `im` objects. If provided, the dimensions of the objects will be adjusted to `ngrid` before the conversion to the array.
+#' @param entire_window a owin object. If given, then the values outside the region will be set to `NA`.
+#' @param ... Additional arguments passed to spatstat conversion functions.
 #' 
 #' @details `imls_to_arr()` is a internal function for `imls_to_vec()`. By default, it returns a three-dimensional array of dimension \eqn{n} by \eqn{m} by\eqn{l} where \eqn{n} and \eqn{m}
-#' are the dimensions of the im objects, and \eqn{l} is the length of the list. All the im objects in the list need to have the same dimensions. 
-
-imls_to_arr <- function(imls,start = 1,end = NULL,entire_window = NULL,ngrid = NULL){
-  adjust_dim <- TRUE
+#' are the dimensions of the im objects, and \eqn{l} is the length of the list. All the im objects in the list need to have the same dimensions.
+imls_to_arr <- function(imls,
+                        start = 1,
+                        end = NULL,
+                        entire_window = NULL,...) {
   
-  if(is.null(end)){
+  
+  if (is.null(end)) {
     end <- length(imls)
   }
   
-  if(is.null(ngrid)){
-    ngrid <- dim(imls[[1]])
-    adjust_dim <- FALSE
-  }
-  if(length(ngrid)==1){
-    ngrid <- rep(ngrid,2)
-  }
+  if (!is.null(entire_window)) {
+    window_matrix <- spatstat.geom::as.matrix.owin(entire_window, ...)
+    window_matrix[window_matrix == 0] <- NA
     
-  if(!is.null(entire_window)){
-    window_matrix <- spatstat.geom::as.matrix.owin(entire_window,dimyx = ngrid)
-    window_matrix[window_matrix==0] <- NA
-    if(adjust_dim){
-      imls <- lapply(start:end, function(x) spatstat.geom::as.matrix.im(spatstat.geom::as.im(imls[[x]],dimyx = ngrid))*window_matrix)
-    }else{
-      imls <- lapply(start:end, function(x) spatstat.geom::as.matrix.im(imls[[x]])*window_matrix)
-    }
     
-  }else{
-    if(adjust_dim){
-      imls <- lapply(start:end, function(x) spatstat.geom::as.matrix.im(spatstat.geom::as.im(imls[[x]],dimyx = ngrid)))
-    }else{
-      imls <- lapply(start:end, function(x) spatstat.geom::as.matrix.im(imls[[x]]))
-    }
+    imls <- lapply(start:end, function(x) {
+      spatstat.geom::as.matrix.im(
+        spatstat.geom::as.im(imls[[x]], ...)
+      ) * window_matrix
+    })
+     
+    
+  } else {
+    
+ 
+    imls <- lapply(start:end, function(x) {
+      spatstat.geom::as.matrix.im(
+        spatstat.geom::as.im(imls[[x]],...)
+      )
+    })
+  
     
   }
   
-  imls <- array(unlist(imls),c(dim(imls[[1]]),end-start+1))
-  
+  imls <- array(unlist(imls), c(dim(imls[[1]]), end - start + 1))
   return(imls)
 }
-
